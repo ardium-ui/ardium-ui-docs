2a61b50642b86d4a8f2b8737b7aa56cd55c31834
joker876 <joker876x@gmail.com>
2024-03-28 20:16:55 +0100

    setup app options, create home & not-found pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './pages/home/home.page';
+import { NotFoundPage } from './pages/not-found/not-found.page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomePage, title: 'Ardium UI Docs' },
+  { path: '**', component: NotFoundPage },
+];
