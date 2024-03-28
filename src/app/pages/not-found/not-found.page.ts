7bb688f14db1e0b8318dc2be908fa0ddb164390c
joker876 <joker876x@gmail.com>
2024-03-28 23:29:44 +0100

    implement better not-found icon & height handling

--- src/app/pages/not-found/not-found.page.ts
+++ src/app/pages/not-found/not-found.page.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ArdiumButtonModule } from '@ardium-ui/ui';
+import { NotFoundSvg } from '@svg';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [RouterModule, ArdiumButtonModule],
+  imports: [RouterModule, ArdiumButtonModule, NotFoundSvg],
   templateUrl: './not-found.page.html',
-  styleUrl: './not-found.page.scss'
+  styleUrl: './not-found.page.scss',
 })
-export class NotFoundPage {
-
-}
+export class NotFoundPage {}
