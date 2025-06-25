b64b68f61a1cf0d6a331d72f2518207e14a78d00
joker876 <joker876x@gmail.com>
2025-06-25 14:55:31 +0200

    Updates

--- chunk-NUGM6ON6.js
+++
@@ -1,1 +0,0 @@
-import{a,b}from"./chunk-PFPSCWCN.js";import"./chunk-5FDIALMT.js";import"./chunk-IWXHOVAQ.js";import"./chunk-FDERIQAA.js";export{b as DEVKIT_ROUTES,a as devkitRouteData};
---
+++ chunk-SBLV52S7.js
@@ -0,0 +1,1 @@
+import{a,b}from"./chunk-YIOCKGNW.js";import"./chunk-5FDIALMT.js";import"./chunk-IWXHOVAQ.js";import"./chunk-FDERIQAA.js";export{b as DEVKIT_ROUTES,a as devkitRouteData};
