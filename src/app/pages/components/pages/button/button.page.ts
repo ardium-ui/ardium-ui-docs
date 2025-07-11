39f8f3614e087c2e2f62bb9aedb09f7abe15564c
joker876 <42680429+joker876@users.noreply.github.com>
2025-07-11 23:05:59 +0200

    mostly finish button page

--- src/app/pages/components/pages/button/button.page.ts
+++ src/app/pages/components/pages/button/button.page.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ArdiumButtonModule } from '@ardium-ui/ui';
 import { ArticleSectionsModule } from 'src/app/components/article-sections/article-sections.module';
 import { CodeExampleComponent } from 'src/app/components/code-example/code-example.component';
 import { HeadingsModule } from 'src/app/components/headings/headings.module';
-import { ButtonBasicExampleData } from './../../../../../../txt-dist/button-basic-example';
+import { ButtonBasicExampleData, ButtonIconExampleData } from 'txt-dist';
 
 @Component({
   selector: 'button-page',
   standalone: true,
-  imports: [CodeExampleComponent, ArticleSectionsModule, HeadingsModule],
-  templateUrl: './button.page.html',
+  imports: [CodeExampleComponent, ArticleSectionsModule, HeadingsModule, ArdiumButtonModule, RouterModule],
+templateUrl: './button.page.html',
   styleUrl: './button.page.scss',
 })
 export class ButtonPage {
   readonly ButtonBasicExampleData = ButtonBasicExampleData;
+  readonly ButtonIconExampleData = ButtonIconExampleData;
 }
---
+++ src/examples/components/button/button-icon-example/button-icon-example.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ArdiumButtonModule } from '@ardium-ui/ui';
+
+@Component({
+  selector: 'button-icon-example',
+  templateUrl: './button-icon-example.html',
+  styleUrl: './button-icon-example.scss',
+  standalone: true,
+  imports: [ArdiumButtonModule],
+})
+export class ButtonIconExample {
+  
+}
