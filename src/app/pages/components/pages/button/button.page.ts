e0e4d6992d6d124b6eeac827ad4dd2b83262dae2
joker876 <42680429+joker876@users.noreply.github.com>
2025-07-15 18:58:54 +0200

    finish button page

--- src/app/pages/components/pages/button/button.page.ts
+++ src/app/pages/components/pages/button/button.page.ts
@@ -3,18 +3,33 @@ import { RouterModule } from '@angular/router';
 import { ArdiumButtonModule } from '@ardium-ui/ui';
 import { ArticleSectionsModule } from 'src/app/components/article-sections/article-sections.module';
 import { CodeExampleComponent } from 'src/app/components/code-example/code-example.component';
+import { CodeComponent } from 'src/app/components/code/code.component';
 import { HeadingsModule } from 'src/app/components/headings/headings.module';
 import { ButtonBasicExampleData, ButtonDisabledExampleData, ButtonIconExampleData } from 'txt-dist';
 
 @Component({
   selector: 'button-page',
   standalone: true,
-  imports: [CodeExampleComponent, ArticleSectionsModule, HeadingsModule, ArdiumButtonModule, RouterModule],
-templateUrl: './button.page.html',
+  imports: [
+    CodeExampleComponent,
+    CodeComponent,
+    ArticleSectionsModule,
+    HeadingsModule,
+    ArdiumButtonModule,
+    RouterModule,
+  ],
+  templateUrl: './button.page.html',
   styleUrl: './button.page.scss',
 })
 export class ButtonPage {
   readonly ButtonBasicExampleData = ButtonBasicExampleData;
   readonly ButtonDisabledExampleData = ButtonDisabledExampleData;
   readonly ButtonIconExampleData = ButtonIconExampleData;
+
+  readonly providingDefaultValuesExampleCode = `export const appConfig: ApplicationConfig = {
+  providers: [
+    // ... other providers
+    provideButtonDefaults({ variant: 'sharp' }), // all buttons will now be sharp by default
+  ],
+};`;
 }
