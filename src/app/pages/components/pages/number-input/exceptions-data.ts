90336dcbc900cfa8827330c772e34a75dce3be7c
joker876 <joker876x@gmail.com>
2026-03-21 20:45:49 +0100

    create number input exceptions and styling data

--- src/app/pages/components/pages/number-input/exceptions-data.ts
+++ src/app/pages/components/pages/number-input/exceptions-data.ts
@@ -26,7 +26,71 @@ export const NumberInputExceptionsData: ExceptionsPageData = {
             {
               code: `<ard-number-input [stepSize]="-5"></ard-number-input>`,
               codeLanguage: SupportedLanguage.HTML,
-              result: `ARD-FT0071b: Cannot set <ard-number-input>'s [stepSize] to a negative value, got -5.`,
+              result: `ARD-FT0071b: Cannot set <ard-number-input>'s [stepSize] to a negative value, got "-5".`,
+            },
+          ],
+        },
+        {
+          code: 'ARD-FT0071c',
+          exceptionText:
+            '<ard-number-input>\'s [stepSize] must be an integer when [allowFloat] is false, got "<i>step size</i>".',
+          description: [
+            'This exception is raised if the <code>[stepSize]</code> input is set to a non-integer value while the <code>[allowFloat]</code> input is set to false.',
+            'When <code>[allowFloat]</code> is false, the <code>[stepSize]</code> input must be an integer to ensure that the number input only allows whole numbers.',
+          ],
+          exampleResults: [
+            {
+              code: `<ard-number-input [stepSize]="0.5" [allowFloat]="false"></ard-number-input>`,
+              codeLanguage: SupportedLanguage.HTML,
+              result: `ARD-FT0071c: <ard-number-input>'s [stepSize] must be an integer when [allowFloat] is false, got "0.5".`,
+            },
+          ],
+        },
+        {
+          code: 'ARD-FT0072a',
+          exceptionText:
+            'Cannot set <ard-number-input>\'s [maxDecimalPlaces] to a negative value, got "<i>maxDecimalPlaces</i>".',
+          description: [
+            'This exception is raised if the <code>[maxDecimalPlaces]</code> input is set to a negative value.',
+            'The <code>[maxDecimalPlaces]</code> input must be a non-negative integer to function correctly.',
+          ],
+          exampleResults: [
+            {
+              code: `<ard-number-input [maxDecimalPlaces]="-5"></ard-number-input>`,
+              codeLanguage: SupportedLanguage.HTML,
+              result: `ARD-FT0072a: Cannot set <ard-number-input>'s [maxDecimalPlaces] to a negative value, got "-5".`,
+            },
+          ],
+        },
+        {
+          code: 'ARD-FT0072b',
+          exceptionText:
+            'Cannot set <ard-number-input>\'s [maxDecimalPlaces] to a non-integer value, got "<i>maxDecimalPlaces</i>".',
+          description: [
+            'This exception is raised if the <code>[maxDecimalPlaces]</code> input is set to a non-integer value.',
+            'The <code>[maxDecimalPlaces]</code> input must be a non-negative integer to function correctly.',
+          ],
+          exampleResults: [
+            {
+              code: `<ard-number-input [maxDecimalPlaces]="0.5"></ard-number-input>`,
+              codeLanguage: SupportedLanguage.HTML,
+              result: `ARD-FT0072b: Cannot set <ard-number-input>'s [maxDecimalPlaces] to a non-integer value, got "0.5".`,
+            },
+          ],
+        },
+        {
+          code: 'ARD-FT0073',
+          exceptionText:
+            '<ard-number-input>\'s [decimalSeparator] must be a single character, got "<i>decimalSeparator</i>".',
+          description: [
+            'This exception is raised if the <code>[decimalSeparator]</code> input is set to a value that is not a single character.',
+            'The <code>[decimalSeparator]</code> input must be a single character to function correctly, otherwise the number syntax handlers may not work as expected.',
+          ],
+          exampleResults: [
+            {
+              code: `<ard-number-input decimalSeparator=",,"></ard-number-input>`,
+              codeLanguage: SupportedLanguage.HTML,
+              result: `ARD-FT0073: <ard-number-input>'s [decimalSeparator] must be a single character, got ",,".`,
             },
           ],
         },
--- src/app/pages/components/pages/number-input/styling-data.ts
+++ src/app/pages/components/pages/number-input/styling-data.ts
@@ -2,5 +2,130 @@ import { StylingPageData } from 'src/app/components/styling-page';
 
 export const NumberInputStylingData: StylingPageData = {
   name: 'Number Input',
-  variables: [],
+  variables: [
+    {
+      name: '--ard-number-input-height',
+      description: 'Height of the number input component.',
+      inheritsFrom: '--ard-form-field-height',
+      default: '2.3125rem',
+    },
+    {
+      name: '--ard-number-input-height-compact',
+      description: 'Height of the number input component in compact mode.',
+      inheritsFrom: '--ard-form-field-height-compact',
+      default: '1.6875rem',
+    },
+    {
+      name: '--ard-number-input-gap',
+      description: 'Gap between internal number input elements.',
+      default: '0.625rem',
+    },
+    {
+      name: '--ard-number-input-font-size',
+      description: 'Font size of the number input component.',
+      inheritsFrom: '--ard-form-field-font-size',
+      default: '1rem',
+    },
+    {
+      name: '--ard-number-input-font-size-compact',
+      description: 'Font size of the number input component in compact mode.',
+      inheritsFrom: '--ard-form-field-font-size-compact',
+      default: '0.875rem',
+    },
+    {
+      name: '--ard-number-input-line-height',
+      description: 'Line height of the number input value.',
+      default: '1.25',
+    },
+    {
+      name: '--ard-number-input-line-height-compact',
+      description: 'Line height of the number input value in compact mode.',
+      default: '1',
+    },
+    {
+      name: '--ard-number-input-font-weight',
+      description: 'Font weight of the number input value.',
+      default: '400',
+    },
+    {
+      name: '--ard-number-input-padding-top',
+      description: 'Top padding of the number input.',
+      inheritsFrom: '--ard-form-field-padding-top',
+      default: '0',
+    },
+    {
+      name: '--ard-number-input-padding-right',
+      description: 'Right padding of the number input.',
+      inheritsFrom: '--ard-form-field-padding-right',
+      default: '0.375rem',
+    },
+    {
+      name: '--ard-number-input-padding-bottom',
+      description: 'Bottom padding of the number input.',
+      inheritsFrom: '--ard-form-field-padding-bottom',
+      default: '0',
+    },
+    {
+      name: '--ard-number-input-padding-left',
+      description: 'Left padding of the number input.',
+      inheritsFrom: '--ard-form-field-padding-left',
+      default: '0.375rem',
+    },
+    {
+      name: '--ard-number-input-padding-top-compact',
+      description: 'Top padding of the number input in compact mode.',
+      inheritsFrom: '--ard-form-field-padding-top-compact',
+      default: '0',
+    },
+    {
+      name: '--ard-number-input-padding-right-compact',
+      description: 'Right padding of the number input in compact mode.',
+      inheritsFrom: '--ard-form-field-padding-right-compact',
+      default: '0.375rem',
+    },
+    {
+      name: '--ard-number-input-padding-bottom-compact',
+      description: 'Bottom padding of the number input in compact mode.',
+      inheritsFrom: '--ard-form-field-padding-bottom-compact',
+      default: '0',
+    },
+    {
+      name: '--ard-number-input-padding-left-compact',
+      description: 'Left padding of the number input in compact mode.',
+      inheritsFrom: '--ard-form-field-padding-left-compact',
+      default: '0.375rem',
+    },
+    {
+      name: '--ard-number-input-color',
+      description: 'Text color of the number input value.',
+      inheritsFrom: '--ard-text',
+      default: 'rgba(0, 0, 0, 100%)',
+    },
+    {
+      name: '--ard-number-input-placeholder-color',
+      description: 'Text color of the number input placeholder.',
+      inheritsFrom: '--ard-text',
+      default: 'rgba(0, 0, 0, 100%)',
+    },
+    {
+      name: '--ard-number-input-placeholder-opacity',
+      description: 'Opacity of the number input placeholder.',
+      default: '60%',
+    },
+    {
+      name: '--ard-number-input-disabled-opacity',
+      description: 'Opacity of the number input when disabled.',
+      default: '50%',
+    },
+    {
+      name: '--ard-number-input-width',
+      description: 'Width of the host <code>ard-number-input</code> element.',
+      default: 'unset',
+    },
+    {
+      name: '--ard-number-input-rounded-border-radius',
+      description: 'Border radius used for the rounded variant of the number input.',
+      default: '8px',
+    },
+  ],
 };
