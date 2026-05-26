5ca933184c62db1de2a85ef94bffd597426b7c52
joker876 <joker876x@gmail.com>
2026-05-26 18:16:05 +0200

    Updates

---
+++ chunk-JHKGIPKK.js
@@ -0,0 +1,1 @@
+import{a,b}from"./chunk-PCYEV7IJ.js";import"./chunk-WUDOSVQC.js";import"./chunk-R43OUQ2O.js";import"./chunk-FDERIQAA.js";export{b as COMPONENT_ROUTES,a as componentRouteData};
---
+++ chunk-WBKZM22T.js
@@ -0,0 +1,1 @@
+import{a,b}from"./chunk-YTX6BZAA.js";import"./chunk-WUDOSVQC.js";import"./chunk-R43OUQ2O.js";import"./chunk-FDERIQAA.js";export{b as DEVKIT_ROUTES,a as devkitRouteData};
